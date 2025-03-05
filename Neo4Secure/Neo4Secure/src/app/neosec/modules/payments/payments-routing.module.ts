36b843ca393fa55d9f7cdd9fc463731cf15ec93c
Gabriel Paz <gpaz@bytesw.com>
2025-03-04 19:03:57 -0600

    Ultimos cambios

--- Neo4Secure/Neo4Secure/src/app/neosec/modules/payments/payments-routing.module.ts
+++ Neo4Secure/Neo4Secure/src/app/neosec/modules/payments/payments-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePaymentsComponent } from './home-payments/home-payments.component';
+import { PaymentHistoryComponent } from './payment-history/payment-history.component';
 
 const routes: Routes = [
-  { path: '', component: HomePaymentsComponent }
+  { path: '', component: HomePaymentsComponent },
+  { path: 'history', component: PaymentHistoryComponent }
 ];
 
 @NgModule({
